71cc1024b9941e8751eaf54ec5c46976c13febad
ZombossXD <124475391+ZombossXD@users.noreply.github.com>
2024-01-27 12:35:26 +0000

    Add files via upload

--- Austin Post Malone Website/form-handler.php
+++ Austin Post Malone Website/form-handler.php
@@ -1,24 +1,28 @@
-?php
+<?php
 $name = $_POST['name'];
 $Visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
+$subjct = $_POST['subject'];
 $message = $_POST['message'];
 
 $email_from = 'info@yourwebsite.com';
 
 $email_subject = 'New Form Submission';
 
-$email_body = "User Name: $name. \n" .
-    "User Email: $Visitor_email. \n" .
-    "Subject: $subject. \n" .
-    "User Message: $message \n";
+$email_body = "User Name: $name. /n".
+			   "User Email: $Visitor_email. /n".
+			    "Subject: $subject. /n".
+			    "User Message: $message ./n";
 
 $to = 'jjwestlake2005@gmail.com';
 
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-to: $Visitor_email \r\n";
+$headers = "From: $email_from /r/n";
 
-mail($to, $email_subject, $email_body, $headers);
+$headers .= "Reply-to: $Visitor_email /r/n";
+
+mail($to,$email_subject,$email_body,$headers);
 
 header("Location: Contact.html");
+
+
+
 ?>
